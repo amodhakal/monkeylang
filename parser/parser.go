ac720556044bb7e19ea1bf58eab7ca69ceaf460b
Amodh Dhakal <amodhakal@gmail.com>
2025-09-15 00:25:56 -0400

    Did read  pseudocode of page 37

---
+++ parser/parser.go
@@ -0,0 +1,29 @@
+package parser
+
+import (
+	"monkeylang/ast"
+	"monkeylang/lexer"
+	"monkeylang/token"
+)
+
+type Parser struct {
+	l            *lexer.Lexer
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
